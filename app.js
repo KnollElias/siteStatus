daab0f238d5b164fd5c6378afa56966de1540028
knollElias <knoll.elias@icloud.com>
2023-12-14 12:14:47 +0100

    Mde it work

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const axios = require('axios');
+const axios = require('axios').default;
 const chalk = require('chalk');
 
 // Array of URLs to check
 const urls = ['http://example.com', 'http://another-example.com'];
 
 urls.forEach(url => {
-    axios.get(url, { timeout: 10000 })
-        .then(response => {
-            if (response.status === 200) {
-                console.log(chalk.green(`${url} responded OK`));
-            } else {
-                console.log(chalk.yellow(`${url} responded with status code: ${response.status}`));
-            }
-        })
-        .catch(error => {
-            if (error.response) {
-                console.log(chalk.yellow(`${url} responded with status code: ${error.response.status}`));
-            } else if (error.request) {
-                console.log(chalk.red(`${url} failed to respond`));
-            } else {
-                console.log(chalk.red(`Error in accessing ${url}: ${error.message}`));
-            }
-        });
+  axios.get(url, { timeout: 10000 })
+    .then(response => {
+      if (response.status === 200) {
+        console.log(chalk.green(`${url} responded OK`));
+      } else {
+        console.log(chalk.yellow(`${url} responded with status code: ${response.status}`));
+      }
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(chalk.yellow(`${url} responded with status code: ${error.response.status}`));
+      } else if (error.request) {
+        console.log(chalk.red(`${url} failed to respond`));
+      } else {
+        console.log(chalk.red(`Error in accessing ${url}: ${error.message}`));
+      }
+    });
 });

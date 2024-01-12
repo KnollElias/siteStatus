bdf78db4e63b1df7645365a6ce61eb7e29930e4a
KnollElias <107244530+KnollElias@users.noreply.github.com>
2024-01-12 18:17:42 +0100

    added real sites

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const axios = require('axios').default;
-const chalk = require('chalk');
+const axios = require("axios").default;
+const chalk = require("chalk");
 
 // Array of URLs to check
-const urls = ['http://example.com', 'http://another-example.com'];
+const urls = [
+  "https://knollelias.ch",
+  "https://peaksolution.ch",
+  "https://barbershopsherifi.ch",
+  "https://erhardtphilipp.ch",
+  "https://knollbackend1.ch/system",
+];
 
-urls.forEach(url => {
-  axios.get(url, { timeout: 10000 })
-    .then(response => {
+urls.forEach((url) => {
+  axios
+    .get(url, { timeout: 10000 })
+    .then((response) => {
       if (response.status === 200) {
         console.log(chalk.green(`${url} responded OK`));
       } else {
-        console.log(chalk.yellow(`${url} responded with status code: ${response.status}`));
+        console.log(
+          chalk.yellow(`${url} responded with status code: ${response.status}`)
+        );
       }
     })
-    .catch(error => {
+    .catch((error) => {
       if (error.response) {
-        console.log(chalk.yellow(`${url} responded with status code: ${error.response.status}`));
+        console.log(
+          chalk.yellow(
+            `${url} responded with status code: ${error.response.status}`
+          )
+        );
       } else if (error.request) {
         console.log(chalk.red(`${url} failed to respond`));
       } else {
